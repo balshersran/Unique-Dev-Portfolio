eb10ce837fac1b71a4fbb1d877f3167e73e611bc
Balsher Sran <balshersran@Balshers-MacBook-Pro.local>
2024-10-21 10:01:39 -0700

    worked on images for the projects cards

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -2,7 +2,7 @@ export default function Project(props) {
     return(
         <div className="container">
             <div className="card-container">
-                <img src={`props.img`} alt="#" />Img:
+                <img src={props.img} alt="#" />
                 <h5 className="card-title">{props.title}</h5>
                 <a href="#">{props.link}</a>
             </div>
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -3,23 +3,33 @@ import Header from "../components/Header";
 
 // each Project card has img, title, github repo link
 const hmtlProj = {
-     title: 'HTML/CSS Portfolio',
+    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRs_nYdI8f24mA9OFtxAOs5m3HPdPzO1WgXHzN5L888BZLEgaj3A7WbO9Gs29VnyIbBxwY&usqp=CAU',
+    title: 'HTML/CSS Portfolio',
+    
 };
 
 const jsEmployeeTrackerProject = {
+    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzI7qYn1JjwMYDLV6GnH4hWDPw4b-U6v7sBDK0o_k5oGTQEhaJIxsOKbt43xLpjf7erwA&usqp=CAU',
     title: 'JavaScript Employee Tracker',
+    link: '',
 };
 
 const vehicleBuilderCLI = {
+    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3mwYL0tHhprquMrMTIEm8S2txxvkUm8Yyvg&s',
    title: 'Super-Duper-Ultra-Unique-Vehicle-Builder',
+   link: '',
 };
 
 const sqlEmployeeTracker = {
+    img: 'https://s3.amazonaws.com/lowres.cartoonstock.com/business-commerce-small_businesses-short-midgets-internet-software_programs-mfl0300_low.jpg',
      title: 'JS/SQL Unique-CLI-CMS',
+     link: '',
 };
 
 const weatherDashboard = {
+     img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQELp9ASf2edw52vBsTqGwsFdr308N4-ZzF8w&s',
      title: 'Superb-Ultra-Unique-Weather-Dashboard',
+     link: '',
 }
 
 export default function Portfolio() {
